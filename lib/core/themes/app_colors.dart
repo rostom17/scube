4d9de7b2b94fed228d471a953a68c480d9e6c706
rostom17 <rostom825@gmail.com>
2026-01-20 22:25:34 +0600

    feat(ui): color theme added

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF0096FC);
+  static const Color secondaryColor = Color(0xFF00C0E8);
+
+  static const Color yelloIconShade = Color(0xFFFCF1DC);
+  static const Color greenIconShade = Color(0xFFDDF4F9);
+  static const Color purpleIconShade = Color(0xFFE8E8F8);
+}
